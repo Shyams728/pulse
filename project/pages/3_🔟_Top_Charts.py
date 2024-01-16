1b7011b7f209db150bd05aa3687465a009b45042
shyamsundardharwad <shyamsundardharwad@gmail.com>
2024-01-16 20:44:23 +0000

    save data

--- project/pages/3_🔟_Top_Charts.py
+++ project/pages/3_🔟_Top_Charts.py
@@ -3,18 +3,18 @@ import streamlit as st
 import pandas as pd
 from sqlalchemy import create_engine
 import streamlit_shadcn_ui as ui
-import extra_streamlit_components as stx
 
 # Set Streamlit page configuration
 st.set_page_config(
     page_title="Top Charts",
     layout="wide",
-    initial_sidebar_state = "collapsed", 
+    initial_sidebar_state="collapsed",
     page_icon=":graph:",
 )
 
 st.markdown('**Top charts**')
-#***************************************Data Access*******************************
+
+# ***************************************Data Access*******************************
 # Create a function to load data from the SQLite database
 def load_data_from_db(query):
     sqlite_file = 'project/test.sqlite'
@@ -25,20 +25,15 @@ def load_data_from_db(query):
 
 def select_the_table(selected_tab):
     # Execute different queries based on the chosen tab
-    if selected_tab == 'Transaction Data':
-        query = 'SELECT * FROM top_transaction_country;'
-    elif selected_tab == 'Insurance Data':
-        query = 'SELECT * FROM top_insurence_country;'
-    elif selected_tab == 'User Data':
-        query = 'SELECT * FROM top_user_country;'
-    elif selected_tab == 'Statewise Transaction Data':
-        query = 'SELECT * FROM top_transaction_state;'
-    elif selected_tab == 'Statewise Insurance Data':
-        query = 'SELECT * FROM top_insurance_state;'
-    elif selected_tab == 'Statewise User Data':
-        query = 'SELECT * FROM top_user_state;'
-    return query
-
+    table_queries = {
+        'Transaction Data': 'SELECT * FROM top_transaction_country;',
+        'Insurance Data': 'SELECT * FROM top_insurence_country;',
+        'User Data': 'SELECT * FROM top_user_country;',
+        'Statewise Transaction Data': 'SELECT * FROM top_transaction_state;',
+        'Statewise Insurance Data': 'SELECT * FROM top_insurance_state;',
+        'Statewise User Data': 'SELECT * FROM top_user_state;'
+    }
+    return table_queries.get(selected_tab, None)
 
 
 def plot_barchart_top(df, x, y, title):
@@ -56,77 +51,76 @@ def plot_barchart_top(df, x, y, title):
     st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
 
-#***************************************Data Exploration*******************************
+# ***************************************Data Exploration*******************************
 first_box = st.container(border=True)
-col1, col2 = first_box.columns([6,1])
+col1, col2 = first_box.columns([6, 1])
 
 with col1:
     # Create tabs
-    selected_tab = ui.tabs(options=['Transaction Data', 'Insurance Data', 'User Data', 'Statewise Transaction Data', 'Statewise Insurance Data', 'Statewise User Data'], default_value='Statewise Insurance Data', key="kanaries")
+    selected_tab = ui.tabs(
+        options=['Transaction Data', 'Insurance Data', 'User Data', 'Statewise Transaction Data',
+                 'Statewise Insurance Data', 'Statewise User Data'], default_value= 'Transaction Data',
+        key="kanaries")
 with col2:
-    filter_data = st.toggle('filter data')
+    filter_data = st.toggle('Filter Data')
 
-choosen_table = select_the_table(selected_tab)
-df = load_data_from_db(choosen_table)
-
-filtered_df = None
-
-
-
-filter_box = st.container(border=True)
-if filter_data:
-    # Create columns
-    col3, col4,col5,col6 = filter_box.columns(4)
-
-    # Sidebar input widgets
-    selected_years = col3.multiselect('Select Year:', sorted(df['year'].unique()),default=2022,help='Select the Year')
-    selected_quarters = col4.multiselect('Select Quarter:', sorted(df['quarter'].unique()),default=[1,2,3,4])
-
-    # Check if 'state' is a column in the DataFrame
-    if 'state' in df.columns:
-        selected_state = col5.multiselect('Select State/UT:', sorted(df['state'].unique()),default = ['karnataka'])
-        entity_type = col6.selectbox('Select Entity Type:', sorted(df['entity_type'].unique()))
-
-        # Filter the DataFrame based on user inputs
-        filtered_df = df[(df['state'].isin(selected_state)) & (df['year'].isin(selected_years)) & (df['quarter'].isin(selected_quarters)) & (df['entity_type'] == entity_type)]
-    else:
-        entity_type = col5.selectbox('Select Entity Type:', sorted(df['entity_type'].unique()))
-
-        # Filter the DataFrame based on user inputs
-        filtered_df = df[(df['year'].isin(selected_years)) & (df['quarter'].isin(selected_quarters)) & (df['entity_type'] == entity_type)]
-
-    
-    # Show filtered data using an expander
-    with st.expander('Show Filtered Full Data'):
-        st.dataframe(filtered_df,use_container_width = True)
-
+chosen_table = select_the_table(selected_tab)
+if chosen_table is None:
+    st.warning("Please select a valid table.")
 else:
-    
-    with st.expander('Show Full Data'):
-            st.dataframe(df,use_container_width = True)
+    df = load_data_from_db(chosen_table)
+    filtered_df = None
 
+    filter_box = st.container(border=True)
+    if filter_data:
+        # Create columns
+        col3, col4, col5, col6 = filter_box.columns(4)
 
+        # Sidebar input widgets
+        selected_years = col3.multiselect('Select Year:', sorted(df['year'].unique()), default=2022,
+                                          help='Select the Year')
+        selected_quarters = col4.multiselect('Select Quarter:', sorted(df['quarter'].unique()), default=[1, 2, 3, 4])
 
-display_box_df_charts = st.container(border=True)
-df_col, charts_col = display_box_df_charts.columns([1,1])
+        # Check if 'state' is a column in the DataFrame
+        if 'state' in df.columns:
+            selected_state = col5.multiselect('Select State/UT:', sorted(df['state'].unique()), default=['karnataka'])
+            entity_type = col6.selectbox('Select Entity Type:', sorted(df['entity_type'].unique()))
 
-if filter_data:
-    data = filtered_df
-else:
-    data = df
+            # Filter the DataFrame based on user inputs
+            filtered_df = df[(df['state'].isin(selected_state)) & (df['year'].isin(selected_years)) & (
+                        df['quarter'].isin(selected_quarters)) & (df['entity_type'] == entity_type)]
+        else:
+            entity_type = col5.selectbox('Select Entity Type:', sorted(df['entity_type'].unique()))
 
-with df_col:
-    st.markdown("**Data**")
-    # Display the filtered DataFrame
-    ui.table(data=data.head(10), maxHeight=700,key="filtered_data_table")
+            # Filter the DataFrame based on user inputs
+            filtered_df = df[(df['year'].isin(selected_years)) & (df['quarter'].isin(selected_quarters)) & (
+                        df['entity_type'] == entity_type)]
 
-with charts_col:    
-    # Plotly bar chart
-    # Check if 'registeredUsers' column exists in the DataFrame
-    if 'registeredUsers' in data.columns:
-        plot_barchart_top(data, 'entity_name', 'registeredUsers', f'Top {selected_tab} Analysis')
+        # Show filtered data using an expander
+        with st.expander('Show Filtered Full Data'):
+            st.dataframe(filtered_df, use_container_width=True)
+
     else:
-        plot_barchart_top(data, 'entity_name', 'amount', f'Top {selected_tab} Analysis')
+        with st.expander('Show Full Data'):
+            st.dataframe(df, use_container_width=True)
 
+    display_box_df_charts = st.container(border=True)
+    df_col, charts_col = display_box_df_charts.columns([1, 1])
 
-#****************************************Data Visualisation****************************
+    if filter_data:
+        data = filtered_df
+    else:
+        data = df
+
+    with df_col:
+        st.markdown(f"**{selected_tab}**")
+        # Display the filtered DataFrame
+        ui.table(data=data.head(6), maxHeight=500, key="filtered_data_table")
+
+    with charts_col:
+        # Plotly bar chart
+        # Check if 'registeredUsers' column exists in the DataFrame
+        y_column = 'registeredUsers' if 'registeredUsers' in data.columns else 'amount'
+        plot_barchart_top(data, 'entity_name', y_column, f'Top {selected_tab} Graph')
+
+# ****************************************Data Visualisation****************************
